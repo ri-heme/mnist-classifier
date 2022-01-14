7667a5d746bf7ce3929b0624d2162ca2112dcf09
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-01-14 17:25:37 +0100

    :sparkles: Add Hydra support for configuration

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,18 +1,38 @@
+from pathlib import Path
+
+import hydra
+import wandb
+from dotenv import find_dotenv
+from omegaconf import DictConfig
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 
 from src.data.dataloaders import dataloaders
 from src.models.cnn import CNN
 
-def train():
-    train_dataloader, test_dataloader = dataloaders()
+CONF_PATH = Path(find_dotenv(), "..", "conf").as_posix()
+WEIGHTS_PATH = Path(find_dotenv(), "..", "models").as_posix()
 
-    model = CNN()
 
-    logger = WandbLogger(project="mnist-classifier", log_model=False)
+@hydra.main(config_path=CONF_PATH, config_name="main")
+def train(config: DictConfig):
+    train_dataloader, test_dataloader = dataloaders(config.batch_size)
 
-    trainer = Trainer(max_epochs=10, logger=logger)
+    # Init model
+    model: CNN = hydra.utils.instantiate(config.model)
+
+    # Init logger
+    logger = WandbLogger(project="mnist-classifier", name=config.name, log_model=False)
+
+    # Init trainer
+    trainer: Trainer = hydra.utils.instantiate(
+        config.training, logger=logger, weights_save_path=WEIGHTS_PATH
+    )
     trainer.fit(model, train_dataloader)
+
+    # Close
+    wandb.finish()
+
 
 if __name__ == "__main__":
     train()
