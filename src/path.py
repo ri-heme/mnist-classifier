df8ccae5864de6ae03a6a8a0347fdb25319b34f1
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-01-18 21:12:25 +0100

    :art: Run isort/black

--- src/path.py
+++ src/path.py
@@ -1,12 +1,14 @@
 __all__ = ["checkpoint_path", "CONF_PATH", "WEIGHTS_PATH"]
 
 from pathlib import Path
+
 from dotenv import find_dotenv
 
 ROOT_PATH = Path(find_dotenv()).parent
 
 CONF_PATH = ROOT_PATH / "conf"
 WEIGHTS_PATH = ROOT_PATH / "models"
+
 
 def checkpoint_path(experiment: str, project: str = "mnist-classifier") -> Path:
     try:
