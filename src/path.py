114c6309496d6e79d1dc162d00c901da786dbb9d
Ricardo Hernández Medina <ricardo.medina@cpr.ku.dk>
2022-01-18 18:16:39 +0100

    :sparkles: Add function to get checkpoint path

--- src/path.py
+++ src/path.py
@@ -1,4 +1,4 @@
-__all__ = ["CONF_PATH", "WEIGHTS_PATH"]
+__all__ = ["checkpoint_path", "CONF_PATH", "WEIGHTS_PATH"]
 
 from pathlib import Path
 from dotenv import find_dotenv
@@ -7,3 +7,9 @@ ROOT_PATH = Path(find_dotenv()).parent
 
 CONF_PATH = ROOT_PATH / "conf"
 WEIGHTS_PATH = ROOT_PATH / "models"
+
+def checkpoint_path(experiment: str, project: str = "mnist-classifier") -> Path:
+    try:
+        return next(WEIGHTS_PATH.joinpath(project, experiment).glob("**/*.ckpt"))
+    except StopIteration:
+        raise FileNotFoundError(f"Checkpoint for experiment ´{experiment}´ not found")
